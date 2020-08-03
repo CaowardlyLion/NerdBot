f792fa61eb7320a96748b1e247dc548c999ea856
CaowardlyLion <kaoerik@gmail.com>
2020-08-03 16:58:33 -0400

    initial commit.

--- src/main/java/me/NerdBot/Main.java
+++ src/main/java/me/NerdBot/Main.java
@@ -1,14 +1,12 @@
 package me.NerdBot;
-import net.dv8tion.jda.*;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import javax.security.*;
 import javax.security.auth.login.LoginException;
+import me.NerdBot.creds;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("NzM5ODQ4NTg1OTgxNzIyNzM2.XygbsA.i5jde2pICgfbETFPXE5siZqm6rQ").build();
+        JDA jda = JDABuilder.createDefault(creds.token).build();
 
     }
 }
